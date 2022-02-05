8d00f498c4dd94cd9c0b69a5bb43fc41ddb6f077
Aerglonus <calebcontreras8@gmail.com>
2022-02-05 16:12:17 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,66 @@
+/* It triggers the GSAP animation */
+PageTransition = () => {
+  var timeline = gsap.timeline();
+  timeline.to("header", {
+    zIndex: 1,
+  });
+
+  timeline.to(".page-transition", {
+    duration: 1,
+    height: "100%",
+    top: "0%",
+  });
+  timeline.to(".page-transition", {
+    duration: 0.8,
+    height: "100%",
+    top: "100%",
+    delay: 0.3,
+  });
+
+  timeline.set(".page-transition", {
+    top: "-100%",
+  });
+};
+
+mainAnimation = () => {
+  var timeline = gsap.timeline();
+
+  timeline.from(".container h1, .menu-items li, .logo", {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    stagger: {
+      amount: 0.4,
+    },
+    delay: 0.8,
+  });
+};
+/* delays the animation */
+delay = (n) => {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+};
+/* makes use of barbajs */
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+        PageTransition();
+        await delay(1000);
+        done();
+      },
+      async enter(data) {
+        mainAnimation();
+      },
+      async once(data) {
+        mainAnimation();
+      },
+    },
+  ],
+});
