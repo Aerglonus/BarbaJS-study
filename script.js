01c33b09a0acc011befe56e3fe6e6e46877033b0
Aerglonus <calebcontreras8@gmail.com>
2022-02-06 21:10:21 -0700

    [feat] Scrolltrigger/scrolltotop/transition

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* It triggers the GSAP animation */
+/* Animates the container that covers the screen using GSAP */
 pageTransition = () => {
   var timeline = gsap.timeline();
   timeline.to(".loading-screen", {
@@ -10,24 +10,59 @@ pageTransition = () => {
     duration: 0.5,
     width: "100%",
     left: "100%",
-    delay: 0.5,
+    delay: 0.2,
   });
 
   timeline.set(".loading-screen", { left: "-100%" });
 };
-
+/* Animation for the content on a scrollable page */
 contentAnimation = () => {
-  var timeline = gsap.timeline();
-
-  timeline.from(".all", {
-    duration: 0.5,
-    y: 30,
-    opacity: 0,
-    stagger: {
-      amount: 0.4,
+  var timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".logo, .botton",
+      start: "20px 80%",
+      end: "+=300",
+      toggleActions: "restart pause none reverse",
     },
-    delay: 0.2,
   });
+  timeline.fromTo(
+    ".logo",
+    {
+      duration: 0.5,
+      y: 100,
+      opacity: 0,
+      stagger: {
+        axis: "y",
+        grid: "auto",
+        ease: "power3.inOut",
+        amount: 4,
+      },
+      delay: 0.5,
+    },
+    {
+      opacity: 1,
+      y: 30,
+    }
+  );
+
+  timeline.fromTo(
+    ".button",
+    {
+      duration: 0.5,
+      y: 100,
+      opacity: 0,
+      stagger: {
+        axis: "y",
+        grid: "auto",
+        ease: "power3.inOut",
+        amount: 4,
+      },
+    },
+    {
+      opacity: 1,
+      y: 30,
+    }
+  );
 };
 
 /* delays the animation */
@@ -39,23 +74,95 @@ delay = (n) => {
     }, n);
   });
 };
-/* makes use of barbajs */
+
+/* makes use of barbajs to trigger GSAP animation*/
 barba.init({
   sync: true,
   transitions: [
     {
+      /*When leave the current page execute the GSAP transition and delay the other animations*/
       async leave(data) {
+        data.current.container.remove();
         const done = this.async();
         pageTransition();
-        await delay(1000);
+        await delay(800);
         done();
       },
+      /*before the transition ends bring the gsap container opacity to 0 */
+      async beforeLeave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+        });
+      },
+      /* before it enters the new page [this case the single page] kill the GSAP scrolltrigger function*/
+      async beforeEnter(data) {
+        ScrollTrigger.getAll().forEach((t) => t.kill());
+      },
+      /*Execute the animations for the next page*/
       async enter(data) {
         contentAnimation();
+        contentAnimation2();
+        return gsap.from(data.next.container, {
+          opacity: 0,
+        });
       },
+      /*Execute the animation once it finish the browser reload*/
       async once(data) {
         contentAnimation();
+        contentAnimation2();
       },
     },
   ],
 });
+
+/* Function to bring back the viewport to the top of the page*/
+function scrollFunction1() {
+  let e = document.getElementById("el1");
+  e.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+    inline: "start",
+  });
+}
+/* GSAP animation for a single page page*/
+contentAnimation2 = () => {
+  var timeline = gsap.timeline({});
+  timeline.fromTo(
+    ".logo1",
+    {
+      duration: 0.5,
+      y: 100,
+      opacity: 0,
+      stagger: {
+        axis: "y",
+        grid: "auto",
+        ease: "power3.inOut",
+        amount: 4,
+      },
+      delay: 0.5,
+    },
+    {
+      opacity: 1,
+      y: 30,
+    }
+  );
+
+  timeline.fromTo(
+    ".button1",
+    {
+      duration: 0.5,
+      y: 100,
+      opacity: 0,
+      stagger: {
+        axis: "y",
+        grid: "auto",
+        ease: "power3.inOut",
+        amount: 4,
+      },
+    },
+    {
+      opacity: 1,
+      y: 30,
+    }
+  );
+};
